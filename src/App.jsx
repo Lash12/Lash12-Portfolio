4a77b343c7d90a13d1c9f3eaea77f9157791b927
Logan <95258701+Lash12@users.noreply.github.com>
2024-06-27 17:08:43 +0000

    Initial edits

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Logan Ash",
+  title: "Web Designer",
+  email: "loganash@outlook.com",
+  gitHub: "Lash12",
+/**   instagram: "microsoft", */
+  linkedIn: "logan-ash-ba0795129/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+/**  twitter: "microsoft", */
+/**   youTube: "Code", */
 };
 
 const primaryColor = "#4E567E";
